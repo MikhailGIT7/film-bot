b08a6b7b2a5d7c4d0bb19db20b8cdaeb3a972cba
MikhailGIT7 <maffin7@gmail.com>
2025-04-14 22:51:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 
 import os
+import logging
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.enums import ParseMode
 from aiohttp import web
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
@@ -14,19 +18,22 @@ if not BOT_TOKEN:
 bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
-# Обработка команды /start через фильтр текста
+# Обработка команды /start
 @dp.message(F.text == "/start")
 async def start_handler(message: types.Message):
+    logger.info("Обработка команды /start")
     await message.answer("Привет! Я помогу подобрать фильм на вечер 🎬")
 
 # Webhook handler
 async def handle_webhook(request: web.Request):
+    logger.info("==> Получен webhook-запрос от Telegram")
     try:
         data = await request.json()
+        logger.info(f"Данные запроса: {data}")
         update = types.Update(**data)
         await dp.feed_update(bot, update)
     except Exception as e:
-        print(f"Error in webhook: {e}")
+        logger.error(f"Ошибка при обработке webhook: {e}")
     return web.Response()
 
 # Запуск aiohttp сервера
@@ -34,5 +41,5 @@ app = web.Application()
 app.router.add_post(f"/webhook/{BOT_TOKEN}", handle_webhook)
 
 if __name__ == "__main__":
-    print(f"Webhook установлен: https://film-bot.onrender.com/webhook/{BOT_TOKEN}")
+    logger.info(f"Webhook установлен: https://film-bot.onrender.com/webhook/{BOT_TOKEN}")
     web.run_app(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
